1c8c0e6f37abf21f2e5128f8e15068219ada64e2
Tec <Tec@SKAB1-PC8>
2024-11-06 11:42:28 +0100

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInspection.Models
+{
+    public class Car : Vehicle 
+    {
+        public Car(string brand, string model, DateTime productionDate, DateTime lastInspection) : base(brand,model,productionDate,lastInspection) 
+        {
+        }
+        public override bool InspectionStatus()
+        {
+            return (DateTime.Now.Year - ProductionDate.Year > 4) && (DateTime.Now.Year - LastInspection.Year > 2);
+        }
+
+        public override string DisplayInfo()
+        {
+            return $"Car: {Brand} {Model}";
+        }
+    }
+}
---
+++ Models/Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInspection.Models
+{
+    public class Truck : Vehicle
+    {
+        public Truck(string brand, string model, DateTime productionDate, DateTime lastInspection) : base(brand, model, productionDate, lastInspection)
+        {
+        }
+        public override bool InspectionStatus()
+        {
+            return (DateTime.Now.Year - ProductionDate.Year > 1) && (DateTime.Now.Year - LastInspection.Year > 1);
+        }
+
+        public override string DisplayInfo()
+        {
+            return $"Truck: {Brand} {Model}";
+        }
+    }
+}
